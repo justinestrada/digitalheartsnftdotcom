16659ebb99a790f0110c8d5a61443e388956f928
justin <jebusiness723@gmail.com>
2022-01-18 02:07:56 -0800

    Tried to use .env vars

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import '../styles/globals.css'
 
 import { MoralisProvider } from 'react-moralis'
 
-const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
-const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
-
 function MyApp({ Component, pageProps }) {
+  const APP_ID = "IEPIlwlUejgQ5F0BufKGybuxI0NX4382tKIV1x4V"
+  const SERVER_URL = "https://ggprzp7ux30w.usemoralis.com:2053/server"
+  // const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID
+  // const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL
+  console.log(APP_ID, SERVER_URL)
 	return (
     <MoralisProvider
       appId={APP_ID}
