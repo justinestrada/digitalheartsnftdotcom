74f8c571d1d7e6a465660d4ebf4f8aaae08d83fa
Justin Estrada <6895474+justinestrada@users.noreply.github.com>
2022-01-26 20:26:51 -0800

    Marketplace - layout template (#1)
    * Marketplace - layout template
    
    * Marketplace - nft card
    
    * NFT meta json files, Styles .border-corners
    
    * Marketplace cards

---
+++ components/Header.js
@@ -0,0 +1,80 @@
+
+import { useMoralis } from 'react-moralis';
+// import { useState } from "react";
+import Login from './Login';
+// import Link from 'next/Link'
+
+const getEllipsisTxt = (str, n = 6) => {
+  if (str) {
+    return `${str.slice(0, n)}...${str.slice(str.length - n)}`
+  }
+  return ''
+}
+
+const onMobileMenuToggle = () => {
+  console.log('on click')
+  // const aria_expanded = $(this).attr('aria-expanded')
+  // $(this).find('.hamburger-close-animated-icon').toggleClass('open')
+  // if (aria_expanded === 'false') {
+  //   $(this).find('.hamburger-close-animated-icon').addClass('open')
+  // } else if (aria_expanded === 'true') {
+  //   $(this).find('.hamburger-close-animated-icon').removeClass('open')
+  // }
+}
+
+function Header () {
+  const { isAuthenticated, account } = useMoralis();
+  return (
+    <nav id="main-nav" className="navbar navbar-expand-lg fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          {/* <Image
+            // loader={myLoader}
+            src="/logo-white.png"
+            alt="Logo"
+            width={40}
+            height={40}
+          /> */}
+          <img
+            // loader={myLoader}
+            src="/logo-white.png"
+            alt="Logo"
+            width={40}
+            height={40}
+          />
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={onMobileMenuToggle}>
+          <div id="main-nav-icon" className="hamburger-close-animated-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ justifyContent: 'end' }}>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Menu Item</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Menu Item</a>
+            </li>
+          </ul>
+          {isAuthenticated ? (
+            <>
+              <button type="button" className="btn btn-primary text-shadow border-corners border-corners_white" data-bs-toggle="modal" data-bs-target="#accountModal">
+                {getEllipsisTxt(account, 6)}
+              </button>                
+            </>  
+          ) : (
+            <Login />
+          )}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
---
+++ pages/marketplace.js
@@ -0,0 +1,233 @@
+
+import Header from '../components/Header';
+import Script from 'next/script'
+import { useMoralis } from 'react-moralis'
+
+export default function Marketplace() {
+  const { isAuthenticated, authenticate } = useMoralis();
+  const sets = [
+    'Festival Classics',
+    'Fruit Loops',
+    'Matrix the Movie',
+    'Lion\'s Den (Mikey Lion Inspired)',
+    'When Pigs Fly (Porky Inspired)',
+    'Dagon Hearts (Asian Inspired Collection)',
+    'Space Cowboy',
+    'Mad Max',
+    'Set 9',
+    'Set 10'
+  ]
+  const items = [
+    {
+      "name": "Bettina Hat Purple",
+      "external_url": "https://digitalheartsnft.com/",
+      "description": "",
+      "image": "http://mydigitalsauce.com/wp-content/uploads/2022/01/Bettina-Hat-Purple.jpg",
+      "attributes": [
+        {
+          "trait_type": "Item Type",
+          "value": "Hat"
+        },
+        {
+          "trait_type": "Hat Type",
+          "value": "Bettina"
+        },
+        {
+          "trait_type": "Color",
+          "value": "Purple"
+        },
+        {
+          "trait_type": "Pattern",
+          "value": "None"
+        }
+      ]
+    },
+    {
+      "name": "Bettina Hat Zebra",
+      "external_url": "https://digitalheartsnft.com/",
+      "description": "",
+      "image": "http://mydigitalsauce.com/wp-content/uploads/2022/01/Bettina-Hat-Zebra.jpg",
+      "attributes": [
+        {
+          "trait_type": "Item Type",
+          "value": "Hat"
+        },
+        {
+          "trait_type": "Hat Type",
+          "value": "Bettina"
+        },
+        {
+          "trait_type": "Color",
+          "value": "White"
+        },
+        {
+          "trait_type": "Pattern",
+          "value": "Zebra"
+        }
+      ]
+    },
+    {
+      "name": "Carly Romper Fruits",
+      "external_url": "https://digitalheartsnft.com/",
+      "description": "",
+      "image": "http://mydigitalsauce.com/wp-content/uploads/2022/01/Carly-Romper-Fruits.jpg",
+      "attributes": [
+         {
+            "trait_type": "Item Type",
+            "value": "Chest"
+         },
+         {
+            "trait_type": "Chest Type",
+            "value": "Romper"
+         },
+         {
+            "trait_type": "Color",
+            "value": "White"
+         },
+         {
+            "trait_type": "Pattern",
+            "value": "Fruits"
+         }
+      ]
+    },
+    {
+      "name": "Daryn Boots Watermelon",
+      "external_url": "https://digitalheartsnft.com/",
+      "description": "",
+      "image": "http://mydigitalsauce.com/wp-content/uploads/2022/01/Daryn-Boots-Watermelon.jpg",
+      "attributes": [
+         {
+            "trait_type": "Item Type",
+            "value": "Shoes"
+         },
+         {
+            "trait_type": "Shoes Type",
+            "value": "Boots"
+         },
+         {
+            "trait_type": "Color",
+            "value": "Red"
+         },
+         {
+            "trait_type": "Pattern",
+            "value": "Watermelon"
+         }
+      ]
+    }
+  ]
+  return (
+    <>
+      <main id="marketplace" style={{backgroundImage: 'url(/bg-colorful.jpg)'}}>
+        <Header />
+        <section id="marketplace_inner-wrap" className="container card px-0">
+          <div className="card-body">
+            <div id="marketplace_stats" className="row mb-4">
+              <div className="col-lg-3">
+                <div className="marketplace_stat border-corners">
+                  <div className="stat_label">Items in Collection</div>
+                  <div className="stat_value">1000</div>
+                </div>
+              </div>
+              <div className="col-lg-3">
+                <div className="marketplace_stat border-corners">
+                  <div className="stat_label">Owners</div>
+                  <div className="stat_value">1000</div>
+                </div>
+              </div>
+              <div className="col-lg-3">
+                <div className="marketplace_stat border-corners">
+                  <div className="stat_label">Floor Price</div>
+                  <div className="stat_value">1 ETH</div>
+                </div>
+              </div>
+              <div className="col-lg-3">
+                <div className="marketplace_stat border-corners">
+                  <div className="stat_label">Total Volume</div>
+                  <div className="stat_value">100 ETH</div>
+                </div>
+              </div>
+            </div>
+            <div id="marketplace_filters" className="row mb-4">
+              <div className="col-lg-4">
+                <div className="form-group">
+                  <label className="text-white">Sets</label>
+                  <select className="form-control">
+                    { sets ? (
+                      sets?.map((item, index) => {
+                        return (
+                          <option key={index}>{item}</option>
+                        )
+                      })
+                    ) : (
+                      <div className="py-5 text-center">Loading...</div>
+                    )}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div id="marketplace_listing">
+              <div className="row">
+                {items.map(function(item, index) {
+                  const is_owner = false
+                  return (
+                    <div className="col-md-4 col-lg-3 mb-4" key={index}>
+                      <div className="card card-nft border-corners">
+                        <div className="card-body">
+                          <div>
+                            <div className="text-white mb-2">{item.attributes[0].value}</div>
+                            <img src={item.image} alt={item.name} className="mb-2"/>
+                          </div>
+                          <div>
+                            <h3 className="text-white text-shadow mb-2">{item.name}</h3>
+                            {isAuthenticated ? (
+                              <>
+                                {is_owner ? (
+                                  <button type="button" className="btn btn-primary">SELL</button>
+                                ) : (
+                                  <button type="button" className="btn btn-primary">BUY</button>
+                                )}
+                              </>
+                            ) : (
+                              <>
+                                <button onClick={authenticate} type="button" className="btn btn-primary">CONNECT</button>
+                              </>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })}                
+              </div>
+              {/*
+              <div className="accordion" id="accordionSets">
+                { sets ? (
+                  sets?.map((item, index) => {
+                    return (
+                      <div className="accordion-item" key={index}>
+                        <h2 className="accordion-header" id={`heading${index}`}>
+                          <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapseOne${index}`} aria-expanded="true" aria-controls={`collapse${index}`}>
+                            {item}
+                          </button>
+                        </h2>
+                        <div id={`collapse${index}`} className="accordion-collapse collapse show" aria-labelledby={`heading${index}`} data-bs-parent="#accordionSets">
+                          <div className="accordion-body">
+                            <p>Lorem ipsum</p>
+                          </div>
+                        </div>
+                      </div>     
+                    )
+                  })
+                ) : (
+                  <div className="py-5 text-center">Loading...</div>
+                )}
+              </div> 
+              */}
+            </div>
+          </div>
+        </section>
+      </main>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></Script>
+    </>
+  )
+}
